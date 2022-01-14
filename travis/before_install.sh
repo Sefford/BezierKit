ee0f945b9679501a3edeae198615b62096fb4899
Pedro Gómez <pedro@goodnotesapp.com>
2022-01-14 11:12:59 +0100

    Add travis support for wasm

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 if [[ $TRAVIS_OS_NAME = 'osx' ]]; then
-  # install macOS prerequistes
-  :
+  if [[ $TRAVIS_JOB_NAME = 'WebAssembly']] then
+    brew install swiftwasm/tap/carton
+  fi
 elif [[ $TRAVIS_OS_NAME = 'linux' ]]; then
   wget https://swift.org/builds/swift-5.3-release/ubuntu1804/${SWIFT_VERSION}/${SWIFT_VERSION}-ubuntu18.04.tar.gz
   tar xzf ${SWIFT_VERSION}-ubuntu18.04.tar.gz
--- travis/script.sh
+++ travis/script.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 
 if [[ $TRAVIS_OS_NAME = 'osx' ]]; then
-	echo "project = $PROJECT, DESTINATION = $DESTINATION"
-	set -o pipefail
-	xcodebuild -version
-	xcodebuild -showsdks
-	# Build Framework in Debug and Run Tests if specified
-	if [ $RUN_TESTS == "YES" ]; then
-		xcodebuild -project "$PROJECT" -scheme "$SCHEME" -sdk "$SDK" -destination "$DESTINATION" -configuration Debug ONLY_ACTIVE_ARCH=NO test | xcpretty -c;
-	else
-		xcodebuild -project "$PROJECT" -scheme "$SCHEME" -sdk "$SDK" -destination "$DESTINATION" -configuration Debug ONLY_ACTIVE_ARCH=NO build | xcpretty -c;
+	if [[ $TRAVIS_JOB_NAME = 'WebAssembly']] then
+    	if [ $RUN_TESTS == "YES" ]; then
+			carton test
+		else
+			carton bundle
+		fi
+  	else
+	  echo "project = $PROJECT, DESTINATION = $DESTINATION"
+		set -o pipefail
+		xcodebuild -version
+		xcodebuild -showsdks
+		# Build Framework in Debug and Run Tests if specified
+		if [ $RUN_TESTS == "YES" ]; then
+			xcodebuild -project "$PROJECT" -scheme "$SCHEME" -sdk "$SDK" -destination "$DESTINATION" -configuration Debug ONLY_ACTIVE_ARCH=NO test | xcpretty -c;
+		else
+			xcodebuild -project "$PROJECT" -scheme "$SCHEME" -sdk "$SDK" -destination "$DESTINATION" -configuration Debug ONLY_ACTIVE_ARCH=NO build | xcpretty -c;
+		fi
+		# Run `pod spec lint` if specified
+		if [ $POD_LINT == "YES" ]; then
+			pod spec lint;
+		fi
 	fi
-  	# Run `pod spec lint` if specified
-  	if [ $POD_LINT == "YES" ]; then
-  		pod spec lint;
-    fi
+	
 elif [[ $TRAVIS_OS_NAME = 'linux' ]]; then
  	export PATH="${PWD}/${SWIFT_VERSION}-ubuntu18.04/usr/bin:${PATH}"
 	swift test --enable-test-discovery
