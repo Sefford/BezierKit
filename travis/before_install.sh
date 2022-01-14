0dacc481ce124a758da78c4f8e8fa14411f8ded6
Pedro Gómez <pedro@goodnotesapp.com>
2022-01-14 12:00:58 +0100

    Try with linux instead

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 if [[ $TRAVIS_OS_NAME = 'osx' ]]; then
-  if [[ $TRAVIS_JOB_NAME = 'WebAssembly' ]]; then
-    brew install swiftwasm/tap/carton
-  fi
+  # install macOS prerequistes
+  :
 elif [[ $TRAVIS_OS_NAME = 'linux' ]]; then
-  wget https://swift.org/builds/swift-5.3-release/ubuntu1804/${SWIFT_VERSION}/${SWIFT_VERSION}-ubuntu18.04.tar.gz
-  tar xzf ${SWIFT_VERSION}-ubuntu18.04.tar.gz
+  if [[ $TRAVIS_JOB_NAME = 'WebAssembly' ]]; then
+    docker run -v `pwd`:`pwd` -w `pwd` ghcr.io/swiftwasm/carton:latest /bin/sh -c 'carton test'
+  else
+    wget https://swift.org/builds/swift-5.3-release/ubuntu1804/${SWIFT_VERSION}/${SWIFT_VERSION}-ubuntu18.04.tar.gz
+    tar xzf ${SWIFT_VERSION}-ubuntu18.04.tar.gz
+  fi
 fi
